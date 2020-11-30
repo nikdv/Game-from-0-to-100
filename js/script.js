ce1098991bfda3a158d8bf2fd001eaa177ba669a
Nikolaev Dmitry <nik.dima001@yandex.ru>
2020-11-30 22:39:50 +0300

    Игровой бот

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+let userNum;
+let randomNum = parseInt(Math.random() * 100);
+
+
+function isNum(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+while (true) {
+    userNum = prompt('Введите число от 0 до 100');
+    if (userNum === null) break;
+
+    if()
+    
+    alert('Игра окончена')
+
+    if (isNum(userNum)) {
+        userNum = +userNum;
+
+        if (userNum > randomNum) {
+            alert('Загаданое число большее');
+        } else if (userNum < randomNum) {
+            alert('Загаданое число меньшее');
+        } else if (userNum === randomNum) {
+            alert('Поздравляю, вы победили!');
+            randomNum = parseInt(Math.random() * 100);
+        }
+    } else {
+        alert('Можно вводить только числа');
+    }
+      
+}
